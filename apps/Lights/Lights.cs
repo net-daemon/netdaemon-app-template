282afa468b0f011fbf30fa49e4c60138f49c0078
Eugene Niemand <eugene.niemand@asos.com>
2021-02-09 08:47:04 +0000

    Turn off lights when sleeping

---
+++ apps/Lights/Lights.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Reactive.Linq;
+using NetDaemon.Common.Reactive;
+
+// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+namespace Niemand
+{
+    /// <summary>
+    ///     Hello world showcase
+    /// </summary>
+    public class Lights : NetDaemonRxApp
+    {
+        public override void Initialize()
+        {
+            Entity("input_select.house_mode")
+                .StateChanges
+                .Where(e => e.New?.State.ToLower() == "sleeping")
+                .Subscribe(_ => Entity("light.downstairs").TurnOff());
+        }
+    }
+}
